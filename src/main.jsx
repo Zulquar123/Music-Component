3017842c0166014d1d93d4be498de12711b371ec
code-with-zulquar <codewithzulquar@gmail.com>
2024-11-30 16:13:41 +0530

    update

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,8 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
